dcfe3ac311b1a38de10ba5f06fc177fdebec04b6
bas <tima.bashirov92@gmail.com>
2024-09-13 13:58:43 +0300

    main was added.

--- cost.py
+++ cost.py
@@ -1,3 +1,6 @@
+from details import choose_detail
+from colours import choose_color
+
 def calculate_cost (color, detail):
     base_cost = 12000
     full_cost = choose_color(color) * choose_detail(detail) * base_cost
---
+++ main.py
@@ -0,0 +1,7 @@
+from cost import calculate_cost
+
+
+details = input("Choose your detail: \n>> ")
+colour = input("Choose your colour: \n>> ")
+
+print(calculate_cost(details, colour))
