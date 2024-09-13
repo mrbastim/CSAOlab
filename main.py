b461dc9a32ed9c69d98dd8331ab89ebe2d31bb7b
bas <tima.bashirov92@gmail.com>
2024-09-13 14:06:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from cost import calculate_cost
 details = input("Choose your detail: \n>> ")
 colour = input("Choose your colour: \n>> ")
 
-print(calculate_cost(details, colour))
+print(calculate_cost(colour, details))
